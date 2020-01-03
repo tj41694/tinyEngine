a758ce2acc2d0b006083b90db5cdc89cc7db8016
tj41694 <32859938+tj41694@users.noreply.github.com>
2020-01-03 17:51:10 +0800

    nm

--- TinyEngine/Engine/Components/ReflectProb.h
+++ TinyEngine/Engine/Components/ReflectProb.h
@@ -4,8 +4,8 @@ namespace TEngine {
 	class Camera;
 	class ReflectProb : public Component {
 	public:
-		unsigned int texSize;
-		unsigned int cubeMap;
+		unsigned int texSize = 0;
+		unsigned int cubeMap = 0;
 	public:
 		ReflectProb();
 		void Start() override;
